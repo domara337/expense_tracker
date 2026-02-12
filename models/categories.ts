42f83796504ddbcfe39fcd9c49b421f332326579
Domara <domara.best@gmail.com>
2026-02-12 11:04:15 +0300

    added findCategoryByid and create

--- models/categories.ts
+++ models/categories.ts
@@ -24,3 +24,25 @@ return result.rows;
 
 
 }
+
+//find category by id
+export const findCategoryById=async(id:number):Promise<Category|null>=>{
+    const result=await pool.query("SELECT * FROM categories WHERE id=$1", 
+        [id]
+    );
+
+    return result.rows[0] || null;
+
+}
+
+
+//create a new category
+export const createCategory=async({
+    name,
+}:CreateCategoryInput):Promise<Category>=>{
+    const result=await pool.query(
+        "INSERT INTO categories (name) VALUES ($1) RETURNING *",
+        [name]
+    );
+    return result.rows[0];
+};
